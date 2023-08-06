16529cc10b0ec25fbc5bf23b422cda5c5fcdfe77
Siedlerchr <siedlerkiller@gmail.com>
2023-08-06 21:37:33 +0200

    add export to

--- jlink-service-b/src/main/java/module-info.java
+++ jlink-service-b/src/main/java/module-info.java
@@ -4,7 +4,7 @@ import org.jdk.sample.jlink.test.service.b.BServiceInterface;
 module org.jdk.sample.jlink.test.service.b {
 
     requires org.jdk.sample.jlink.test.service;
-    exports org.jdk.sample.jlink.test.service.b;
+    exports org.jdk.sample.jlink.test.service.b to org.jdk.sample.jlink.test.service;
 
     provides ServiceInterface
             with BServiceInterface;
